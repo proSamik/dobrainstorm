61c640101c69bc41c89df653e75fa4c82904b20c
Samik Choudhury <samikchoudhury15@gmail.com>
2025-04-25 18:48:33 +0530

    Update Demo.tsx

--- client/src/components/landing/Demo.tsx
+++ client/src/components/landing/Demo.tsx
@@ -39,7 +39,7 @@ export function Demo() {
           <iframe
             width="100%"
             height="100%"
-            src="https://www.youtube.com/embed/cn8MsmLzOQQ?autoplay=0&rel=0&modestbranding=1&controls=1&showinfo=0&iv_load_policy=3&disablekb=1&fs=1&cc_load_policy=0"
+            src="https://www.youtube.com/embed/jhcqzejnKWg?autoplay=0&rel=0&modestbranding=1&controls=1&showinfo=0&iv_load_policy=3&disablekb=1&fs=1&cc_load_policy=0"
             title="Product Demo"
             loading="lazy"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
